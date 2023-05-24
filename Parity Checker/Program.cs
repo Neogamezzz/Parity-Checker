b9378acd63c685b9efcd67b4c3bd12dcb40d2351
Neogamezzz <lyosha555@outlook.com>
2023-05-24 20:36:43 +0200

    Add project files.

---
+++ Parity Checker/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Parity_Checker
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter a number to check its parity.");
+
+            while (true)
+            {
+                Console.Write("Your number: ");
+                if (!int.TryParse(Console.ReadLine(), out int number))
+                {
+                    Console.WriteLine("Invalid value. Try again.");
+                    continue;
+                }
+                int remainder = number % 2;
+                
+                if (remainder == 0)
+                {
+                    Console.WriteLine("This number is even");
+                }
+                else
+                {
+                    Console.WriteLine("This number is odd");
+                }
+            }
+        }
+    }
+}
